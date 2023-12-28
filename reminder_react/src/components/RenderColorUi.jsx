24b6b490ecaeced9dbd957d780e9e264d3b38498
hongthu <hongthu24032001@gmail.com>
2023-12-28 14:54:36 +0700

    Update UI Color

--- reminder_react/src/components/RenderColorUi.jsx
+++ reminder_react/src/components/RenderColorUi.jsx
@@ -3,20 +3,24 @@ import "../style/style.css";
 class RenderColorOnUi extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { selectedColor: null };
   }
   handleColorClick = (selectedColor) => {
+    this.setState({ selectedColor });
     this.props.onColorClick(selectedColor);
   };
 
   render() {
     const { colors } = this.props;
+    const { selectedColor } = this.state;
     return (
       <>
         {colors.map((color, index) => (
           <div
             key={index}
-            className="color-swatch"
+            className={`color-swatch ${
+              selectedColor === color ? "selected" : ""
+            }`}
             style={{ backgroundColor: color }}
             onClick={() => this.handleColorClick(color)}
           ></div>
